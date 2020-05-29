68a83703bc90dd5b18bc7e38cfeeaaf4cb1e0699
haohunghan <anhha@DESKTOP-U7ERBM5>
2020-05-29 18:46:41 +0700

    apply navigation for storage

--- src/screen/storage/StorageDetailScreen.js
+++ src/screen/storage/StorageDetailScreen.js
@@ -1,10 +1,12 @@
 import {Text, View} from 'react-native';
 import * as React from 'react';
 
-export default function StorageDetailScreen() {
+export default function StorageDetailScreen({route, navigate}) {
+  const {storageId} = route.params;
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
       <Text>Storage detail screen</Text>
+      <Text>{storageId}</Text>
     </View>
   );
 }
--- src/screen/storage/StorageScreen.js
+++ src/screen/storage/StorageScreen.js
@@ -1,14 +1,33 @@
-import {Button, Text, View} from 'react-native';
+import {ListItem} from 'react-native-elements';
+import {Text, View} from 'react-native';
 import * as React from 'react';
+
+const storages = [
+  {
+    name: 'Kho hang loi',
+    id: 'loi',
+  },
+  {
+    name: 'Nguyen Tri Phuong',
+    id: 'ntp',
+  },
+];
 
 export default function StorageScreen({navigation}) {
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Products screen</Text>
-      <Button
-        title="Go to storage detail"
-        onPress={() => navigation.navigate('StorageDetail')}
-      />
+    <View>
+      {storages.map((storage, index) => (
+        <ListItem
+          key={index}
+          title={storage.name}
+          bottomDivider
+          onPress={() => {
+            navigation.navigate('StorageDetail', {
+              storageId: storage.id,
+            });
+          }}
+        />
+      ))}
     </View>
   );
 }
